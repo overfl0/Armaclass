e228e0e9fe4b5e63ab2dcb6332ee4a291585cde5
Lukasz Taczuk <overfl0@users.noreply.github.com>
2018-11-09 02:22:49 +0100

    Nicer formatting

--- armaclass/arma_generator.py
+++ armaclass/arma_generator.py
@@ -1,36 +1,49 @@
+import textwrap
+
 from .generator import Generator
 
 
 class ArmaGenerator(Generator):
     def generate_float(self, name, data):
-        if name is None:
+        if name is None:  # We're in an array, we don't have a name. Don't print a newline at the end
             return '{}'.format(data)
 
         return '{}={};\n'.format(name, data)
 
     def generate_int(self, name, data):
-        if name is None:
+        if name is None:  # We're in an array, we don't have a name. Don't print a newline at the end
             return '{}'.format(data)
 
         return '{}={};\n'.format(name, data)
 
     def generate_class(self, name, data):
         inner = [self.generate_item(key, val) for key, val in data.items()]
+        template = textwrap.dedent('''\
+            class {name}
+            {{
+            {contents}}};
+            ''')
 
-        retval = 'class {}\n{{\n{}}};\n'.format(name, self._indent(''.join(inner)))
+        retval = template.format(name=name, contents=self._indent(''.join(inner)))
         return retval
 
     def generate_array(self, name, data):
         inner = [self.generate_item(None, val) for val in data]
+        template = textwrap.dedent('''\
+            {name}[]=
+            {{
+            {contents}
+            }};
+            ''')
 
-        retval = '{}[]=\n{{\n{}\n}};\n'.format(name, self._indent(','.join(inner)))
+        retval = template.format(name=name, contents=self._indent(','.join(inner)))
         return retval
 
     def _escape_string(self, data):
         return data.replace('"', '""')
 
     def generate_string(self, name, data):
-        if name is None:
+        if name is None:  # We're in an array, we don't have a name. Don't print a newline at the end
             return '"{}"'.format(self._escape_string(data))
 
         return '{}="{}";\n'.format(name, self._escape_string(data))
