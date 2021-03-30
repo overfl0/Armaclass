c2140e397db7b4f3007dc0c0d83d955c6a71e98e
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-03-31 00:59:19 +0200

    Tests style fix

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,5 +1,3 @@
-import textwrap
-
 import pytest
 
 from armaclass import parse
@@ -67,18 +65,19 @@ def test_plus_array():
 
 
 def test_ignore_symbols():
-    testString = ("class Moo {\n"
-                  "\tfoo = xxx;\n"
-                  "\tclass xxx {};\n"
-                  "};"
-                  )
+    test_string = (
+        'class Moo {\n'
+        '\tfoo = xxx;\n'
+        '\tclass xxx {};\n'
+        '};'
+    )
     with pytest.raises(RuntimeError):
-        parse(testString)
+        parse(test_string)
 
 
 def test_ignore_inheritance():
-    testString = 'class Moo : foo {};'
-    assert parse(testString) == {'Moo': {}}
+    test_string = 'class Moo : foo {};'
+    assert parse(test_string) == {'Moo': {}}
 
 
 def test_line_comments():
@@ -89,11 +88,11 @@ def test_line_comments():
 
 
 def test_multiline_comments():
-    assert parse("/* foo comment*/") == {}
-    assert parse("/* foo comment\nsomething */x=2;") == {'x': 2}
-    assert parse("x=2;/* foo comment*/") == {'x': 2}
-    assert parse("x/*asd*/=/**/2;/* foo comment*/") == {'x': 2}
-    assert parse("class Moo { /* foo comment*/};") == {'Moo': {}}
+    assert parse('/* foo comment*/') == {}
+    assert parse('/* foo comment\nsomething */x=2;') == {'x': 2}
+    assert parse('x=2;/* foo comment*/') == {'x': 2}
+    assert parse('x/*asd*/=/**/2;/* foo comment*/') == {'x': 2}
+    assert parse('class Moo { /* foo comment*/};') == {'Moo': {}}
 
 
 def test_quote_escaping_by_double_quote():
@@ -102,123 +101,208 @@ def test_quote_escaping_by_double_quote():
 
 def test_sample():
     expected = {
-        "Session": {
-            "Player1": {
-                "customScore": 0,
-                "killed": 0,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 4,
-                "killsPlayers": 0,
-                "killsSoft": 0,
-                "killsTotal": 4,
-                "name": "Lord DK"
+        'Session': {
+            'Player1': {
+                'customScore': 0,
+                'killed': 0,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 4,
+                'killsPlayers': 0,
+                'killsSoft': 0,
+                'killsTotal': 4,
+                'name': 'Lord DK'
             },
-            "Player2": {
-                "customScore": 0,
-                "killed": 0,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 3,
-                "killsPlayers": 0,
-                "killsSoft": 0,
-                "killsTotal": 3,
-                "name": "XiviD"
+            'Player2': {
+                'customScore': 0,
+                'killed': 0,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 3,
+                'killsPlayers': 0,
+                'killsSoft': 0,
+                'killsTotal': 3,
+                'name': 'XiviD'
             },
-            "Player3": {
-                "customScore": 0,
-                "killed": 0,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 2,
-                "killsPlayers": 0,
-                "killsSoft": 0,
-                "killsTotal": 2,
-                "name": "40mm2Die"
+            'Player3': {
+                'customScore': 0,
+                'killed': 0,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 2,
+                'killsPlayers': 0,
+                'killsSoft': 0,
+                'killsTotal': 2,
+                'name': '40mm2Die'
             },
-            "Player4": {
-                "customScore": 0,
-                "killed": 0,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 4,
-                "killsPlayers": 0,
-                "killsSoft": 0,
-                "killsTotal": 4,
-                "name": "WickerMan"
+            'Player4': {
+                'customScore': 0,
+                'killed': 0,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 4,
+                'killsPlayers': 0,
+                'killsSoft': 0,
+                'killsTotal': 4,
+                'name': 'WickerMan'
             },
-            "Player5": {
-                "customScore": 0,
-                "killed": 1,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 3,
-                "killsPlayers": 0,
-                "killsSoft": -1,
-                "killsTotal": 1,
-                "name": "Fusselwurm"
+            'Player5': {
+                'customScore': 0,
+                'killed': 1,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 3,
+                'killsPlayers': 0,
+                'killsSoft': -1,
+                'killsTotal': 1,
+                'name': 'Fusselwurm'
             },
-            "Player6": {
-                "customScore": 0,
-                "killed": 0,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 0,
-                "killsPlayers": 0,
-                "killsSoft": 0,
-                "killsTotal": 0,
-                "name": "Simmax"
+            'Player6': {
+                'customScore': 0,
+                'killed': 0,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 0,
+                'killsPlayers': 0,
+                'killsSoft': 0,
+                'killsTotal': 0,
+                'name': 'Simmax'
             },
-            "Player7": {
-                "customScore": 0,
-                "killed": 2,
-                "killsAir": 0,
-                "killsArmor": 0,
-                "killsInfantry": 0,
-                "killsPlayers": 0,
-                "killsSoft": 0,
-                "killsTotal": 0,
-                "name": "Andre"
+            'Player7': {
+                'customScore': 0,
+                'killed': 2,
+                'killsAir': 0,
+                'killsArmor': 0,
+                'killsInfantry': 0,
+                'killsPlayers': 0,
+                'killsSoft': 0,
+                'killsTotal': 0,
+                'name': 'Andre'
             },
-            "duration": 5821.1724,
-            "gameType": "Coop",
-            "island": "Altis",
-            "mission": "W-CO@10 StealBoot v03"
+            'duration': 5821.1724,
+            'gameType': 'Coop',
+            'island': 'Altis',
+            'mission': 'W-CO@10 StealBoot v03'
         }
     }
-    result = parse("\n\tclass Session\n\t{\n\tmission=\"W-CO@10 StealBoot v03\";\n\tisland=\"Altis\";\n\t" +
-                   "gameType=\"Coop\";\n\tduration=5821.1724;\n\tclass Player1\n\t{\n\tname=\"Lord DK\";\n\tkillsInfantry=4;\n\t" +
-                   "killsSoft=0;\n\tkillsArmor=0;\n\tkillsAir=0;\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=4;\n\tkilled=0;" +
-                   "\n\t};\n\tclass Player2\n\t{\n\tname=\"XiviD\";\n\tkillsInfantry=3;\n\tkillsSoft=0;\n\tkillsArmor=0;\n\tkillsAir=0;" +
-                   "\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=3;\n\tkilled=0;\n\t};\n\t" +
-                   "class Player3\n\t{\n\tname=\"40mm2Die\";\n\tkillsInfantry=2;\n\tkillsSoft=0;\n\tkillsArmor=0;\n\tkillsAir=0;" +
-                   "\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=2;\n\tkilled=0;\n\t};\n\t" +
-                   "class Player4\n\t{\n\tname=\"WickerMan\";\n\tkillsInfantry=4;\n\tkillsSoft=0;\n\tkillsArmor=0;\n\tkillsAir=0;" +
-                   "\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=4;\n\tkilled=0;\n\t};\n\t" +
-                   "class Player5\n\t{\n\tname=\"Fusselwurm\";\n\tkillsInfantry=3;\n\tkillsSoft=-1;\n\tkillsArmor=0;\n\tkillsAir=0;" +
-                   "\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=1;\n\tkilled=1;\n\t};\n\t" +
-                   "class Player6\n\t{\n\tname=\"Simmax\";\n\tkillsInfantry=0;\n\tkillsSoft=0;\n\tkillsArmor=0;\n\tkillsAir=0;" +
-                   "\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=0;\n\tkilled=0;\n\t};\n\t" +
-                   "class Player7\n\t{\n\tname=\"Andre\";\n\tkillsInfantry=0;\n\tkillsSoft=0;\n\tkillsArmor=0;\n\tkillsAir=0;" +
-                   "\n\tkillsPlayers=0;\n\tcustomScore=0;\n\tkillsTotal=0;\n\tkilled=2;\n\t};\n\t};\n\n\t")
+    result = parse('''
+    class Session
+    {
+        mission="W-CO@10 StealBoot v03";
+        island="Altis";
+        gameType="Coop";
+        duration=5821.1724;
+
+        class Player1
+        {
+            name="Lord DK";
+            killsInfantry=4;
+            killsSoft=0;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=4;
+            killed=0;
+        };
+        class Player2
+        {
+            name="XiviD";
+            killsInfantry=3;
+            killsSoft=0;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=3;
+            killed=0;
+        };
+        class Player3
+        {
+            name="40mm2Die";
+            killsInfantry=2;
+            killsSoft=0;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=2;
+            killed=0;
+        };
+        class Player4
+        {
+            name="WickerMan";
+            killsInfantry=4;
+            killsSoft=0;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=4;
+            killed=0;
+        };
+        class Player5
+        {
+            name="Fusselwurm";
+            killsInfantry=3;
+            killsSoft=-1;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=1;
+            killed=1;
+        };
+        class Player6
+        {
+            name="Simmax";
+            killsInfantry=0;
+            killsSoft=0;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=0;
+            killed=0;
+        };
+        class Player7
+        {
+            name="Andre";
+            killsInfantry=0;
+            killsSoft=0;
+            killsArmor=0;
+            killsAir=0;
+            killsPlayers=0;
+            customScore=0;
+            killsTotal=0;
+            killed=2;
+        };
+    };
+    ''')
     assert result == expected
 
 
 def test_multiline_init():
-    source = textwrap.dedent(r'''
-        class Item0 {
-            position[]={1954.6425,5.9796591,5538.1045};
-            id=0;
-            init="[this, ""Platoon""] call FP_fnc_setVehicleName;" \n "if (isServer) then {" \n "  [this] call FP_fnc_clearVehicle; this addWeaponCargoGlobal [""CUP_launch_M136"", 1];" \n "  this addMagazineCargoGlobal [""1Rnd_HE_Grenade_shell"", 10];" \n "  this addMagazineCargoGlobal [""ATMine_Range_Mag"", 6];" \n "};";
-        };
-    ''')
+    source = (
+        r'class Item0 {'
+        r'    position[]={1954.6425,5.9796591,5538.1045};'
+        r'    id=0;'
+        r'    init="[this, ""Platoon""] call FP_fnc_setVehicleName;" \n "if (isServer) then {" \n "  [this] call '
+        r'FP_fnc_clearVehicle; this addWeaponCargoGlobal [""CUP_launch_M136"", 1];" \n "  this '
+        r'addMagazineCargoGlobal [""1Rnd_HE_Grenade_shell"", 10];" \n "  this addMagazineCargoGlobal '
+        r'[""ATMine_Range_Mag"", 6];" \n "};";'
+        r'};'
+    )
 
     result = parse(source)
     expected = {
         'Item0': {
             'position': [1954.6425, 5.9796591, 5538.1045],
             'id': 0,
-            'init': '[this, "Platoon"] call FP_fnc_setVehicleName;\nif (isServer) then {\n  [this] call FP_fnc_clearVehicle; this addWeaponCargoGlobal ["CUP_launch_M136", 1];\n  this addMagazineCargoGlobal ["1Rnd_HE_Grenade_shell", 10];\n  this addMagazineCargoGlobal ["ATMine_Range_Mag", 6];\n};'
+            'init': '[this, "Platoon"] call FP_fnc_setVehicleName;\nif (isServer) then {\n  [this] call '
+                    'FP_fnc_clearVehicle; this addWeaponCargoGlobal ["CUP_launch_M136", 1];\n  this '
+                    'addMagazineCargoGlobal ["1Rnd_HE_Grenade_shell", 10];\n  this addMagazineCargoGlobal '
+                    '["ATMine_Range_Mag", 6];\n};'
         }
     }
     assert result == expected
