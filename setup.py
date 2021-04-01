1607166cec022191a8622c9ef8300faa4b2c6d99
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-04-02 00:09:54 +0200

    Cython things

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+import os
+
 from setuptools import setup
+from Cython.Build import cythonize
 
 # read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -34,6 +36,8 @@ setup(
 
         'License :: OSI Approved :: MIT License',
     ],
+    ext_modules=cythonize(os.path.join('armaclass', 'parser.py'),
+                          compiler_directives={'language_level': '3'}),
 )
 
 # Install in "editable mode" for development:
