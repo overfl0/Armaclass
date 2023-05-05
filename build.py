1abfe0455e00e1c73140ba27aebd49343cd54ed7
Lukasz Taczuk <overfl0@users.noreply.github.com>
2023-05-05 17:41:38 +0200

    Ensure you check the return value for bulding

--- build.py
+++ build.py
@@ -9,6 +9,6 @@ files_to_delete = chain(*(glob.glob(os.path.join(armaclass_path, file_type)) for
 for file_path in files_to_delete:
     os.remove(file_path)
 
-subprocess.run('python setup_cython.py build_ext --inplace --force', shell=True)
-subprocess.run('pytest -x -s', shell=True)
-subprocess.run('python testconfig.py', shell=True)
+subprocess.run('python setup_cython.py build_ext --inplace --force', shell=True, check=True)
+subprocess.run('pytest -x -s', shell=True, check=True)
+subprocess.run('python testconfig.py', shell=True, check=True)
